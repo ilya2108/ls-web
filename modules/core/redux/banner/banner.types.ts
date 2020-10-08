a8952fccc27ad2ea20370b6bed4d664abc860f71
Tuan Le <tuanldt@outlook.com>
2020-10-08 20:14:19 +0200

    delete banner

--- modules/core/redux/banner/banner.types.ts
+++
@@ -1,7 +0,0 @@
-const BannerActionTypes = {
-    CLOSE_BANNER: "CLOSE_BANNER",
-    PASSWORD_CHANGE_SUCCESS: "PASSWORD_CHANGE_SUCCESS",
-    PASSWORD_CHANGE_ERROR: "PASSWORD_CHANGE_ERROR",
-};
-
-export default BannerActionTypes;
