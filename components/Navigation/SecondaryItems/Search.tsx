5ac0fba54fa55c60ae21eb57115568d6e093ceeb
Tuan Le <tuanldt@outlook.com>
2020-10-09 22:13:09 +0200

    search icon highlighted when selected

--- components/Navigation/SecondaryItems/Search.tsx
+++ components/Navigation/SecondaryItems/Search.tsx
@@ -1,8 +1,9 @@
 import { Search as AKSearch } from "@atlaskit/atlassian-navigation";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { openSearchDrawer } from "../../../modules/core/redux/drawer/drawer.actions";
 
 const Search = () => {
+  const isOpen = useSelector(state => state.drawers.searchDrawer)
   const dispatch = useDispatch();
   const dispatchOpenSearchDrawer = () => dispatch(openSearchDrawer());
 
@@ -12,6 +13,7 @@ const Search = () => {
       placeholder="Search..."
       tooltip="Search"
       label="Search"
+      isSelected={isOpen}
     />
   );
 };
