2fcc193d7609ca171eea16c19b470bb0b75a3d0a
Tomáš Kalabis <Tomas Kalabis>
2021-03-29 11:47:59 +0200

    Student's Dashboard - preparation for data fetching

--- components/Dashboards/StudentsDashboard.tsx
+++ components/Dashboards/StudentsDashboard.tsx
@@ -17,104 +17,48 @@ type Props = {
 export default function StudentsDashboard(props: Props) {
     const score = calculateSemesterScore(props.userData.assignments);
 
+    // representative data - TODO fetch data here
+    const data = {
+        overallPercentileHistory: [14, 35, 66, 54, 68, 73, 72, 65, 71, 78, 81, 82, 85],
+        overallMedianHistory: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55],
+        scoreHistory: [12, 56, 89, 57, 47, 69, 69, 48, 37, 63, 59, 67, 88],
+        scoreHistogram: {
+            label: ["0", "3", "5", "7", "7.5", "8", "11", "14", "19", "24", "25", "26", "27", "28", "28", "34",
+                    "35", "36", "39", "41", "42", "44", "45.5", "46", "48", "50", "51", "53", "54", "55", "68",
+                    "75", "77", "81", "85", "91"],
+            frequency: [12, 19, 30, 55, 201, 37, 12, 19, 30, 55, 201, 37, 12, 19, 30, 55, 201, 37, 12, 19, 30, 55,
+                        201, 37, 12, 19, 30, 55, 201, 37, 12, 19, 30, 55, 201, 37]
+        },
+        assignments: {
+            assignmentName: ["awk1", "awk2", "sed DU", "grep", "grep DU"],
+            assignmentScore: [0, 2, 1, 0, 2],
+            assignmentPercentile: [10, 15, 23, 30, 37],
+            assignmentMedian: [4, 3, 2, 4, 6],
+            assignmentMaxScore: [5, 4, 4, 6, 8]
+        }
+    }
+
   return (
     <Dashboard>
       <InfoBannersContainer>
         <InfoBanner text={"Score:"} value={score} />
-        <InfoBanner text={"Percentil:"} value={"hardcoded"} />
-        <InfoBanner text={"Median:"} value={"hardcoded"} />
-        <InfoBanner text={"Grade:"} value={"hardcoded"} />
+        <InfoBanner text={"Percentile:"} value={data.overallPercentileHistory[data.overallPercentileHistory.length - 1]} />
+        <InfoBanner text={"Median:"} value={data.overallMedianHistory[data.overallMedianHistory.length - 1]} />
       </InfoBannersContainer>
-      <PieChart
-        title={"Last year's students result"}
-        data={{
-          datasets: [[12, 19, 30, 55, 201, 37]],
-          label: ["A", "B", "C", "D", "E", "F"],
-        }}
-        description={"Chart shows final grades of last course students with similar score in the same time"}
-      />
+      {/*<PieChart*/}
+      {/*  title={"Last year's students result"}*/}
+      {/*  data={{*/}
+      {/*    datasets: [[12, 19, 30, 55, 201, 37]],*/}
+      {/*    label: ["A", "B", "C", "D", "E", "F"],*/}
+      {/*  }}*/}
+      {/*  description={"Chart shows final grades of last course students with similar score in the same time"}*/}
+      {/*/>*/}
       <BarChart
         title={"Students' Score Histogram"}
         description={"Histogram of students score"}
         data={{
-          datasets: [
-            [
-              12,
-              19,
-              30,
-              55,
-              201,
-              37,
-              12,
-              19,
-              30,
-              55,
-              201,
-              37,
-              12,
-              19,
-              30,
-              55,
-              201,
-              37,
-              12,
-              19,
-              30,
-              55,
-              201,
-              37,
-              12,
-              19,
-              30,
-              55,
-              201,
-              37,
-              12,
-              19,
-              30,
-              55,
-              201,
-              37,
-            ],
-          ],
-          label: [
-            "0",
-            "3",
-            "5",
-            "7",
-            "7.5",
-            "8",
-            "11",
-            "14",
-            "19",
-            "24",
-            "25",
-            "26",
-            "27",
-            "28",
-            "28",
-            "34",
-            "35",
-            "36",
-            "39",
-            "41",
-            "42",
-            "44",
-            "45.5",
-            "46",
-            "48",
-            "50",
-            "51",
-            "53",
-            "54",
-            "55",
-            "68",
-            "75",
-            "77",
-            "81",
-            "85",
-            "91",
-          ],
+          datasets: [data.scoreHistogram.frequency],
+          label: data.scoreHistogram.label,
           datasetNames: [""]
         }}
       />
@@ -122,10 +66,7 @@ export default function StudentsDashboard(props: Props) {
         title={"History of Median"}
         description={"Chart shows history of median of all students score and compares it to my score"}
         data={{
-          datasets: [
-            [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55],
-            [12, 56, 89, 57, 47, 69, 69, 48, 37, 63, 59, 67, 88],
-          ],
+          datasets: [data.scoreHistory, data.overallMedianHistory],
           label: [
             "week 1",
             "week 2",
@@ -145,10 +86,10 @@ export default function StudentsDashboard(props: Props) {
         }}
       />
       <LineChart
-        title={"History of Percentil"}
-        description={"Chart shows history of my percentil"}
+        title={"History of Percentile"}
+        description={"Chart shows history of my percentile"}
         data={{
-          datasets: [[14, 35, 66, 54, 68, 73, 72, 65, 71, 78, 81, 82, 85]],
+          datasets: [data.overallPercentileHistory],
           label: [
             "week 1",
             "week 2",
@@ -164,21 +105,21 @@ export default function StudentsDashboard(props: Props) {
             "week 12",
             "week 13",
           ],
-          datasetNames: ["Percentil history"]
+          datasetNames: ["Percentile history"]
         }}
       />
-      <EnumBanner title={"Least successful assignment"}
+      <EnumBanner title={"Assignments"}
         data={{
-          headers: ["Assignment name", "My Score", "Percentil", "Median"],
-          rows: [
-              ["awk1", 0, 10, 4],
-              ["awk2", 2, 15, 4],
-              ["sed DU", 1, 23, 4],
-              ["grep", 0, 30, 4],
-              ["grep DU", 2, 37, 4]
-          ]
+          headers: ["Assignment name", "My Score", "Percentile", "Median", "Max Score"],
+          rows:
+            data.assignments.assignmentName.map((item, i) => [
+                data.assignments.assignmentName[i],
+                data.assignments.assignmentScore[i],
+                data.assignments.assignmentPercentile[i],
+                data.assignments.assignmentMedian[i],
+                data.assignments.assignmentMaxScore[i]])
         }}
-        defaultSortKey={"Percentil"}
+        defaultSortKey={"Percentile"}
         defaultSortOrder={"ASC"}
       />
     </Dashboard>
